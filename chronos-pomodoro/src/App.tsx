40fa7647f9b8c244e3d2dcd1df85ac9d899198eb
Wilson Lima <wilson.lima.silva@icloud.com>
2025-09-18 15:29:35 -0300

    Adcionado primeiro componente tsx

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+export function App() {
+  console.log('oi');
 
-function App() {
-  const [count, setCount] = useState(0)
-
+  //retorna sempre um elemento pai, se tiver mais de dois elementos da ruim
+  //<></> //react fragment tag vazia nao retorna div na inpeção do console
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>Olá Mundo</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et nostrum
+        nobis facilis accusantium fugit aperiam ab saepe ipsa, alias dolorum
+        incidunt reiciendis dolore quidem nihil harum minus debitis magni
+        recusandae!
       </p>
     </>
-  )
+  );
 }
 
-export default App
+//export { App }; // te ogriga a importar usando nome do componente APp
+//export default App; no componete se importa usando qualquer nome
